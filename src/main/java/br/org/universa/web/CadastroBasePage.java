c8bdb45b39d3a3a7cb7c3d9c1e24f3eb86a0d866
victordgt <victordgt@gmail.com>
2010-10-18 19:12:44 -0200

    adicionar página de confirmação de cadastro salvo (não salva de verdade ainda!) e colocar validação nos campos para serem obrigatórios, caso não forem preenchidos, apresenta uma mensagem ao usuário

--- src/main/java/br/org/universa/web/CadastroBasePage.java
+++ src/main/java/br/org/universa/web/CadastroBasePage.java
@@ -10,6 +10,7 @@ import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.PropertyModel;
 
 import br.org.universa.negocio.CadastroBase;
@@ -32,23 +33,25 @@ public class CadastroBasePage extends WebPage {
 	@SuppressWarnings("serial")
 	public CadastroBasePage(final PageParameters parameters) {
 		
+		//PAINEL COM AS MENSAGENS DE VALIDAÇÃO DO FORMULÁRIO
+		add(new FeedbackPanel("feedback")); 
+		
      	Form<Object> form = new Form<Object>("form");
     	form.add(new TextField<String>("titulo", new PropertyModel<String>(this,"cadastroBase.titulo")));
     	
     	DropDownChoice<String> choice=new DropDownChoice<String>("categoria", new PropertyModel<String>(this, "cadastroBase.categoria"), categorias);
+    	choice.setRequired(true);
     	form.add(choice);
     	
-    	form.add(new TextField<String>("nome", new PropertyModel<String>(this,"cadastroBase.nome")));
-    	form.add(new TextField<String>("versao", new PropertyModel<String>(this,"cadastroBase.versao")));
-    	form.add(new TextField<String>("descricao", new PropertyModel<String>(this, "cadastroBase.descricao")));
-    	form.add(new TextField<String>("solucao", new PropertyModel<String>(this, "cadastroBase.solucao")));
-    	
+    	form.add(new TextField<String>("nome", new PropertyModel<String>(this,"cadastroBase.nome")).setRequired(true));
+    	form.add(new TextField<String>("versao", new PropertyModel<String>(this,"cadastroBase.versao")).setRequired(true));
+    	form.add(new TextField<String>("descricao", new PropertyModel<String>(this, "cadastroBase.descricao")).setRequired(true));
+    	form.add(new TextField<String>("solucao", new PropertyModel<String>(this, "cadastroBase.solucao")).setRequired(true));
     	
     	form.add(new Button("cadastra") {
     		@Override
     		public void onSubmit() {
-    			// TODO Auto-generated method stub
-    			super.onSubmit();
+    			setResponsePage(PaginaConfirmacaoCadastroBaseSalvo.class);
     		}
     		
     	});
